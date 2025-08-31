6a80b08e1706a7d1389f0aa3bc972f6713addaf8
Author <geroliabryan024@gmail.com>
2025-08-31 15:34:24 +0800

    added some photo and stuff in homepage

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -6,11 +6,10 @@
 	];
 	// Drop down wala pa
 	let showDropdown = false;
-	
 </script>
 
 <nav
-	class="bg-black text-white px-6 py-4 flex justify-between items-center font-creato uppercase font-bold text-base"
+	class="bg-black text-white px-15 py-5 flex justify-between items-center font-creato font-normal uppercase"
 >
 	<ul class="flex gap-6">
 		{#each links as link}
@@ -20,17 +19,18 @@
 		{/each}
 	</ul>
 	<!-- ! Support dropdown ON HOLD -->
-	
-	<img src="src\lib\assets\logo\LOGO-4.png" alt="Logo" class="logo-4" />
 
+	<img src="src\lib\assets\logo\LOGO-4.png" alt="Logo" class="logo-4"/>
+
+	<!-- Search and Cart -->
 	<div class="flex items-center gap-4">
 		<a href="/account" class="hover:text-mabini-beige">Account</a>
 		<a href="/search" class="relative group">
-			<img src="src\lib\assets\icons\search.png" alt="Search" class="h-6" />
+			<img src="src\lib\assets\icons\search.png" alt="Search" class="h-4 w-5" />
 			<span class="icon-hover"></span>
 		</a>
 		<a href="/cart" class="relative group mr">
-			<img src="src\lib\assets\icons\cart.png" alt="Cart" class="h-6" />
+			<img src="src\lib\assets\icons\cart.png" alt="Cart" class="h-4 w-5" />
 			<span class="icon-hover"> </span>
 		</a>
 	</div>
