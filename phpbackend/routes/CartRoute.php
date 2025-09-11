3bdacbcdcf6b91248cd8dfa47dab48812c032147
Kenneth Domdom <domdomkenneth23@gmail.com>
2025-09-11 11:34:55 +0800

    completing the cartRoute

--- phpbackend/routes/CartRoute.php
+++ phpbackend/routes/CartRoute.php
@@ -5,13 +5,29 @@ $controller = new CartController($db);
 
 switch ($method) {
     case 'GET':
+       $customer_id = $_GET['customer_id'] ?? null;
+
+
+         if($customer_id){
+           
+            $controller->showCartByCustomerId($customer_id);
+        
+         }else if(!$customer_id){
+            $id ? $controller->show($id) : $controller->index();
+              
+         }
+         else{
+             http_response_code(404);
+             echo json_encode(["error"=>"Endpoint not found"]);
+         }
+
         $id ? $controller->show($id) : $controller->index();
         break;
     case 'POST':
         $controller->store();
         break;
     case 'PUT':
-        
+        $controller->update($id);
         break;
     case 'DELETE':
         if($id){
