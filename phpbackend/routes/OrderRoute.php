9f0e933ed4169fb1a3afdfa53bd3156407b914fc
Kenneth Domdom <domdomkenneth23@gmail.com>
2025-09-11 18:02:21 -0700

    OrderRoute Completed

--- phpbackend/routes/OrderRoute.php
+++ phpbackend/routes/OrderRoute.php
@@ -16,13 +16,31 @@ switch ($method) {
     
         break;
      case 'POST':
-    
+       $controller->store();
         break;
      case 'PUT':
-    
+        switch ($subresource) {
+         case 'preparing':
+             $controller->updateToPreparing($id);
+            break;
+         case 'delivering':
+             $controller->updateToDelivering($id);
+             break;
+         case 'completed':
+            $controller->updateToCompleted($id);
+            break;
+         case 'cancelled':
+            $controller->updateToCancelled($id);
+            break;
+         default:
+             http_response_code(405);
+             echo json_encode(["error" => "Method not allowed"]);
+             break;
+        }   
+
         break;
      case 'DELETE':
-     
+          $controller->delete($id);
         break;
     default:
         http_response_code(405);
