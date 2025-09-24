8a0bae051ca34fbb806f729a76de9aa11ca8d5c3
ikennot <domdomkenneth23@gmail.com>
2025-09-24 11:08:55 +0800

    finished OrderItems

--- phpbackend/Controllers/OrderItemsController.php
+++ phpbackend/Controllers/OrderItemsController.php
@@ -71,6 +71,10 @@ class OrderItemsController{
     }
    }
 
+//     
+
+
+
      /**
  * @OA\Get(
  *     path="/mabini-cafe/phpbackend/routes/orderitems?orderId={orderId}",
@@ -78,14 +82,14 @@ class OrderItemsController{
  *     summary="Get all Orderitems by id",
  * @OA\Parameter(
  *         name="orderId",
- *         in="path",
+ *         in="query",
  *         required=true,
  *         description="ID ng menu Order item",
  *         @OA\Schema(type="integer", example=3)
  *     ),
  *     @OA\Response(
  *         response=200,
- *         description="List of all Orderitems"
+ *         description="List of all Orderitems based on order id"
  *     ),
  *     @OA\Response(
  *         response=500,
@@ -93,6 +97,7 @@ class OrderItemsController{
  *     )
  * )
  */
+
   
    public function showByOrderId($orderId){
     $orderItem = $this->model->getByOrderId($orderId);
@@ -107,16 +112,51 @@ class OrderItemsController{
 
 
 
+   
+/**
+ * @OA\Post(
+ *     path="/mabini-cafe/phpbackend/routes/orderitems",
+ *     tags={"Order Items"},
+ *     summary="Create a new orderItem",
+ *     description="Creates a new orderitem",
+ *     @OA\RequestBody(
+ *         required=true,
+ *         description="Order data in JSON format",
+ *         @OA\JsonContent(
+ *             type="object",
+ *             required={"order_id","menu_item_id","quantity","price","subtotal"},
+ *             @OA\Property(property="order_id", type="integer", example=1),  
+ *             @OA\Property(property="menu_item_id", type="integer", example=1),
+ *             @OA\Property(property="quantity", type="integer", example=1),
+ *             @OA\Property(property="price", type="number", format="float", example=1.0),
+ *             @OA\Property(property="subtotal", type="number", format="float", example=250.75)
+ *         )
+ *     ),
+ *     @OA\Response(
+ *         response=201,
+ *         description="Order item created successfully"
+ *     )
+ * )
+ */
+
+
+
+
+
    //create orderItem
    public function store(){
 
      $data = json_decode(file_get_contents("php://input"), true);
-
+     
      if(in_array(null,$data,true) || in_array('',$data,true)){
          http_response_code(400);
          echo json_encode(["error" => "Incomplete data"]);
      }else{
-
+          $this->model->order_id = $data['order_id'];
+           $this->model->menu_item_id = $data['menu_item_id'];
+           $this->model->quantity = $data['quantity'];
+           $this->model->price = $data['price'];
+           $this->model->subtotal = $data['subtotal'];
         if($this->model->create($data)){
             http_response_code(201);
             echo json_encode(["message"=>"OrderItems created successfully"]);
