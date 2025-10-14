77f80f0f58ffb9a904bb5ad4d80b66576b63d536
Author <geroliabryan024@gmail.com>
2025-10-14 13:06:28 +0800

    hamburger update

--- src/lib/components/layout/Header.svelte
+++ src/lib/components/layout/Header.svelte
@@ -50,30 +50,6 @@
 <nav
 	class="flex items-center justify-between p-4 bg-black text-white font-medium uppercase drop-shadow-2xl w-full"
 >
-	<!-- Mobile Hamburger -->
-	<div class="md:hidden flex items-center">
-		<button
-			aria-label="Open menu"
-			class="focus:outline-none"
-			on:click={() => (mobileMenuOpen = !mobileMenuOpen)}
-		>
-			<svg
-				class="w-8 h-8 text-white"
-				fill="none"
-				stroke="currentColor"
-				viewBox="0 0 24 24"
-				xmlns="http://www.w3.org/2000/svg"
-			>
-				<path
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					stroke-width="2"
-					d="M4 6h16M4 12h16M4 18h16"
-				/>
-			</svg>
-		</button>
-	</div>
-
 	<!-- Left links (desktop) -->
 	<div class="hidden md:flex flex-1 justify-start ml-[50px]">
 		<ul class="flex gap-6 items-center">
@@ -126,11 +102,50 @@
 	</div>
 
 	<!-- Centered logo -->
-	<div class="flex-grow flex justify-center">
-		<a href="/">
-			<img src="/images/LOGO-4.png" alt="Logo" class="logo-4 w-[120px] md:w-[180px]" />
+	<div class="flex-grow flex items-center justify-center relative">
+		<a href="/" class="mx-auto">
+			<img
+				src="/images/LOGO-4.png"
+				alt="Logo"
+				class="logo-4 w-[120px] md:w-[180px] justify-self-center"
+			/>
 		</a>
+		<!-- hamburger -->
+		<div class="md:hidden flex items-center absolute right-0 p-2">
+			<button
+				aria-label={mobileMenuOpen ? 'Close menu' : 'Open menu'}
+				class="focus:outline-none"
+				on:click={() => (mobileMenuOpen = !mobileMenuOpen)}
+			>
+				<svg
+					class="w-8 h-8 text-white transition-all duration-300"
+					fill="none"
+					stroke="currentColor"
+					viewBox="0 0 24 24"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					{#if mobileMenuOpen}
+						<!-- X icon -->
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M6 18L18 6M6 6l12 12"
+						/>
+					{:else}
+						<!-- Hamburger icon -->
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M4 6h16M4 12h16M4 18h16"
+						/>
+					{/if}
+				</svg>
+			</button>
+		</div>
 	</div>
+	<!-- Mobile Hamburger -->
 
 	<!-- Right-aligned links (desktop) -->
 	<div class="hidden md:flex flex-1 justify-end gap-4 mr-[50px]">
@@ -195,41 +210,193 @@
 			<span class="underline-anim"></span>
 		</button>
 	</div>
-
-	<!-- TODO: Need pa lagyan ng black bg yung hamburger menu -->
 	<!-- Mobile menu -->
 	{#if mobileMenuOpen}
-		<div class="fixed inset-0 bg-black bg-opacity-80 z-50 flex flex-col">
-			<div class="flex justify-between items-center p-4">
+		<div
+			class="flex gap-0 flex-col items-start bg-black text-white w-full absolute top-[64px] left-0 z-50"
+		>
+			<!-- Home Menu and About -->
+			<div class="w-[250px] grow">
+				<ul class="list-none p-0 m-0">
+					{#each links as link}
+						<li class="w-full">
+							<a
+								class="text-[20px] font-bold w-full block px-6 py-3 bg-black text-white"
+								href={link.href}
+								on:click={() => (mobileMenuOpen = false)}>{link.name}</a
+							>
+						</li>
+					{/each}
+				</ul>
+			</div>
+			<!-- Support Menu -->
+			<div class="w-full grow">
+				<button
+					on:click={() => (open = !open)}
+					class="text-[20px] font-bold w-full px-6 py-3 pr-10 bg-black text-white flex items-center justify-between"
+				>
+					<span class="flex-1 text-left">SUPPORT</span>
+					<span class="transition-transform duration-300 {open ? 'rotate-180' : ''}"> ˅ </span>
+				</button>
+
+				{#if open}
+					<ul class=" bg-black text-white rounded shadow text-[18px] w-full">
+						<li>
+							<button
+								type="button"
+								on:click={() => {
+									open = false;
+									mobileMenuOpen = false;
+									goto('/orders-and-payment');
+								}}
+								class="w-full text-left px-6 py-3 cursor-pointer hover:bg-mabini-yellow hover:text-mabini-dark-brown"
+								>Orders & Payment</button
+							>
+						</li>
+						<li>
+							<button
+								type="button"
+								on:click={() => {
+									open = false;
+									mobileMenuOpen = false;
+									goto('/shipping');
+								}}
+								class="w-full text-left px-6 py-3 cursor-pointer hover:bg-mabini-yellow hover:text-mabini-dark-brown"
+								>Shipping</button
+							>
+						</li>
+					</ul>
+				{/if}
+			</div>
+			<!-- Login -->
+			<div class="w-full grow">
+				{#if $authStore.loading}
+					<div class="text-[20px] opacity-50 w-full px-6 py-3 bg-black text-white rounded">
+						Loading...
+					</div>
+				{:else if $authStore.isAuthenticated}
+					<button
+						class="text-[20px] font-bold w-full px-6 py-3 pr-10 bg-black text-white rounded flex items-center justify-between"
+						on:click={() => {
+							accountOpen = !accountOpen;
+						}}
+					>
+						<span class="flex-1 text-left">ACCOUNT</span>
+						<span class="transition-transform duration-300 {accountOpen ? 'rotate-180' : ''}">
+							˅
+						</span>
+					</button>
+					{#if accountOpen}
+						<ul class="mt-2 bg-black text-white rounded shadow text-[18px] w-full">
+							<li>
+								<button
+									type="button"
+									class="w-full text-left px-6 py-3 cursor-pointer hover:bg-mabini-yellow hover:text-mabini-dark-brown"
+									on:click={() => {
+										accountOpen = false;
+										mobileMenuOpen = false;
+										goto('/settings');
+									}}
+								>
+									Settings
+								</button>
+							</li>
+							<li>
+								<button
+									type="button"
+									class="w-full text-left px-6 py-3 cursor-pointer hover:bg-mabini-yellow hover:text-mabini-dark-brown"
+									on:click={() => {
+										accountOpen = false;
+										mobileMenuOpen = false;
+										logout();
+									}}
+								>
+									Logout
+								</button>
+							</li>
+						</ul>
+					{/if}
+				{:else}
+					<a
+						href="/login"
+						class="text-[20px] font-bold w-full block px-6 py-3 bg-black text-white"
+						on:click={() => (mobileMenuOpen = false)}>Login</a
+					>
+				{/if}
+			</div>
+			<!-- Search and Cart -->
+			<div class="grow w-full px-6 py-3 gap-4 pb-5 items-center flex justify-center">
+				<button
+					class="relative group"
+					on:click={() => {
+						openSearch();
+						mobileMenuOpen = false;
+					}}
+					type="button"
+				>
+					<img src="/icons/search.png" alt="Search" class="h-7 w-8" />
+				</button>
+				<button
+					class="relative group"
+					on:click={() => {
+						openCart();
+						mobileMenuOpen = false;
+					}}
+					type="button"
+				>
+					<img src="/icons/cart.png" alt="Cart" class="h-7 w-8" />
+					{#if $cartCount > 0}
+						<span
+							class="absolute -top-2 -right-2 bg-mabini-yellow text-mabini-dark-brown text-xs font-bold rounded-full h-5 w-5 flex items-center justify-center"
+							>{$cartCount}</span
+						>
+					{/if}
+				</button>
+			</div>
+		</div>
+	{/if}
+</nav>
+
+<CartModal isOpen={cartModalOpen} onClose={closeCart} />
+<SearchModal isOpen={searchModalOpen} onClose={closeSearch} />
+
+<!-- 
+
+
+<div class="fixed inset-0 bg-black flex flex-col">
+			<div class="flex justify-center items-center p-4 pb-1 w-full">
 				<a href="/">
 					<img src="/images/LOGO-4.png" alt="Logo" class="w-[120px]" />
 				</a>
-				<button
-					aria-label="Close menu"
-					class="focus:outline-none"
-					on:click={() => (mobileMenuOpen = false)}
-				>
-					<svg
-						class="w-8 h-8 text-white"
-						fill="none"
-						stroke="currentColor"
-						viewBox="0 0 24 24"
-						xmlns="http://www.w3.org/2000/svg"
+
+				<div class="flex-1">
+					<button
+						aria-label="Close menu"
+						class="focus:outline-none"
+						on:click={() => (mobileMenuOpen = false)}
 					>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width="2"
-							d="M6 18L18 6M6 6l12 12"
-						/>
-					</svg>
-				</button>
+						<svg
+							class="w-8 h-8 text-white"
+							fill="none"
+							stroke="currentColor"
+							viewBox="0 0 24 24"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								stroke-width="2"
+								d="M6 18L18 6M6 6l12 12"
+							/>
+						</svg>
+					</button>
+				</div>
 			</div>
-			<ul class="flex flex-col gap-6 items-center mt-8 w-full">
+			<ul class="flex flex-col gap-0 pt-0 items-center w-full">
 				{#each links as link}
 					<li class="w-full">
 						<a
-							class="text-[20px] font-bold w-full block px-6 py-3 bg-black text-white rounded"
+							class="text-[20px] font-bold w-full block px-6 py-3 bg-black text-white"
 							href={link.href}
 							on:click={() => (mobileMenuOpen = false)}>{link.name}</a
 						>
@@ -238,13 +405,13 @@
 				<li class="w-full">
 					<button
 						on:click={() => (open = !open)}
-						class="text-[20px] font-bold w-full px-6 py-3 bg-black text-white rounded flex items-center justify-between"
+						class="text-[20px] font-bold w-full px-6 py-3 bg-black text-white flex items-center justify-between"
 					>
 						SUPPORT
 						<span class="transition-transform duration-300 {open ? 'rotate-180' : ''}"> ˅ </span>
 					</button>
 					{#if open}
-						<ul class="mt-2 bg-black text-white rounded shadow text-[18px] w-full">
+						<ul class=" bg-black text-white rounded shadow text-[18px] w-full">
 							<li>
 								<button
 									type="button"
@@ -322,12 +489,12 @@
 					{:else}
 						<a
 							href="/login"
-							class="text-[20px] font-bold w-full block px-6 py-3 bg-black text-white rounded"
+							class="text-[20px] font-bold w-full block px-6 py-3 bg-black text-white"
 							on:click={() => (mobileMenuOpen = false)}>Login</a
 						>
 					{/if}
 				</li>
-				<li class="w-full flex gap-4 justify-center">
+				<li class="bg-black p-4 w-full flex gap-4 justify-center">
 					<button
 						class="relative group"
 						on:click={() => {
@@ -357,8 +524,10 @@
 				</li>
 			</ul>
 		</div>
-	{/if}
-</nav>
 
-<CartModal isOpen={cartModalOpen} onClose={closeCart} />
-<SearchModal isOpen={searchModalOpen} onClose={closeSearch} />
+
+
+
+
+
+-->
